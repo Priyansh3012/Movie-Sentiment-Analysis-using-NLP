12bb4416f8a3ea86ab8bd440461130ee01678d75
Priyansh Agrawal <apriyansh47@gmail.com>
2024-08-04 16:46:47 +0530

    Update tokenizer.py

--- tokenizer.py
+++ tokenizer.py
@@ -6,6 +6,5 @@ from nltk.tokenize import word_tokenize
 class LemmaTokenizer:
     def __init__(self):
         self.wordnetlemma = WordNetLemmatizer()
-
     def __call__(self, reviews):
         return [self.wordnetlemma.lemmatize(word) for word in word_tokenize(reviews)]
